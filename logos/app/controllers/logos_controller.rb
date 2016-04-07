305254e7ea99e89aacc46bd3952c6390ae1d7b9a
CJ <dennischoi@Denniss-MacBook.local>
2016-04-07 16:14:34 -0400

    Refractoring to save new logos in database

--- logos/app/controllers/logos_controller.rb
+++ logos/app/controllers/logos_controller.rb
@@ -8,11 +8,22 @@ class LogosController < ApplicationController
   end
 
   def new
+    @logo = Logo.new
   end
 
   def create
-    render :text => "Saving a Logo. URL: #{params[:url]}, Team: #{params[:team]}, Conference: #{params[:conference]}"
+    # render :text => "Saving a Logo. URL: #{params[:url]}, Team: #{params[:team]}, Conference: #{params[:conference]}"
+    @logo = Logo.new(logo_params)
+    if @logo.save
+      redirect_to logos_url
+    else
+      render :new
+    end
   end
 
+  private
+  def logo_params
+    params.require(:logo).permit(:team, :conference, :url)
+  end
 
 end
