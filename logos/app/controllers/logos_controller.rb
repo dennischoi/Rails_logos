9eae5639abad628a416d68f669189a9356c52241
CJ <dennischoi@Denniss-MacBook.local>
2016-04-07 16:10:55 -0400

    Adding feature to create new logo using HTML form

--- logos/app/controllers/logos_controller.rb
+++ logos/app/controllers/logos_controller.rb
@@ -7,4 +7,12 @@ class LogosController < ApplicationController
     @logo = Logo.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    render :text => "Saving a Logo. URL: #{params[:url]}, Team: #{params[:team]}, Conference: #{params[:conference]}"
+  end
+
+
 end
