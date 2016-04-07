7175151cd63a8c36c34dfa2868cb96fcedbedd7d
CJ <dennischoi@Denniss-MacBook.local>
2016-04-07 15:34:34 -0400

    seeds.rb and rake db:seeds

--- logos/db/seeds.rb
+++ logos/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Logo.create!(
+  :team => "New York Knicks",
+  :conference => "East",
+  :url => "http://content.sportslogos.net/logos/6/216/full/7095_new_york_knicks-primary-1981.png"
+)
+
+Logo.create!(
+  :team => "Chicago Bulls",
+  :conference => "East",
+  :url => "http://content.sportslogos.net/logos/6/221/full/hj3gmh82w9hffmeh3fjm5h874.png"
+)
+
+Logo.create!(
+  :team => "Raptors",
+  :conference => "East",
+  :url => "http://content.sportslogos.net/logos/6/227/full/kqmesaae0efg3atwugmeohxga.png"
+)
